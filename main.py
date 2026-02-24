a049c0a2a21885df82a3f21aa3f196808f24ac78
unknown <elian111@gmail.com>
2026-02-23 23:47:01 -0400

    setting basico

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame,sys
+
+class Game:
+    def __init__(self):
+        pass
+    
+    def run(self):
+        pass
+
+    def events(self):
+        pass
+
+    def draw(self):
+        pass
